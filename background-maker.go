eb127a987b10ac446aa5f29a08b573ba3f7cf593
lalala <i@qwq.net>
2022-12-24 17:42:00 +0800

    embed

--- background-maker.go
+++ background-maker.go
@@ -1,14 +1,23 @@
 package background_maker
 
 import (
+	_ "embed"
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
 	"github.com/orzmoe/background-maker/pkg/trianglify"
 )
 
+//go:embed "PingFang SC Bold.ttf"
+var fontEmbed []byte
+
 func BackgroundMaker(text string) (*gg.Context, error) {
-	fontFace, err := gg.LoadFontFace("../PingFang SC Bold.ttf", 48)
+	font, err := freetype.ParseFont(fontEmbed)
 	if err != nil {
 		return nil, err
 	}
+	fontFace := truetype.NewFace(font, &truetype.Options{
+		Size: 42,
+	})
 	return trianglify.New(800, 450, 3000, text, fontFace, nil)
 }
